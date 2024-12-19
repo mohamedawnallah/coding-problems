a2c0065039a72a97cbbfee9f1e2f2065b9be47f5
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2024-12-20 01:59:01 +0200

    batch1: add 169-majority-element

---
+++ batch1/169-majority-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate, count = None, 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if candidate == num else -1)
+        return candidate
