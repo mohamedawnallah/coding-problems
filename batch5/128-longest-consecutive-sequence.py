15c38e9326b532744991e0e749b5acaca708e755
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-04 17:17:40 +0200

    batch5: update 128-longest-consecutive-sequence.py

--- batch5/128-longest-consecutive-sequence.py
+++ batch5/128-longest-consecutive-sequence.py
@@ -1,2 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums_set = set(nums)
+        longest_streak = 0
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                current_streak = 1
+                while num + 1 in nums_set:
+                    num += 1
+                    current_streak += 1
+                
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
