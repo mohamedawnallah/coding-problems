cad9f8856a92b12ba95c1411e3bb47af5bb03d50
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-04 17:18:19 +0200

    batch5: update 56-merge-intervals.py

--- batch5/56-merge-intervals.py
+++ batch5/56-merge-intervals.py
@@ -1,2 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        
+        result = [intervals[0]]
+        for i in range(1, len(intervals)):
+            # Not Overlapped.
+            if intervals[i][0] > result[-1][1]:
+                result.append(intervals[i])
+            
+            # Overlapped.
+            if intervals[i][0] <= result[-1][1]:
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+            
+        return result
