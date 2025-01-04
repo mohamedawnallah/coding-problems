c6c0acb4fae68adca93121b5b7502c60cd48af08
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-04 17:18:01 +0200

    batch5: update 228-summary-ranges.py

--- batch5/228-summary-ranges.py
+++ batch5/228-summary-ranges.py
@@ -1,2 +1,21 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        
+        ranges = []
+        start = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]+1:
+                if nums[i-1] == nums[start]:
+                    ranges.append(str(nums[start]))
+                else:
+                    ranges.append(str(nums[start]) + "->" + str(nums[i-1]))
+                start = i
+        
+        if nums[-1] == nums[start]:
+            ranges.append(str(nums[-1]))
+        else:
+            ranges.append(str(nums[start]) + "->" + str(nums[-1]))
+        
+        return ranges
