4386e54a7575f4d3f10b94fa9be90a79a8fdb9e2
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-10 23:03:59 +0200

    batch6: update 21-merge-two-sorted-lists.py

--- batch6/21-merge-two-sorted-lists.py
+++ batch6/21-merge-two-sorted-lists.py
@@ -5,4 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-	pass   
+        dump = ListNode()
+        head = dump
+        l1, l2 = list1, list2
+        while l1 or l2:
+            op1 = l1.val if l1 else float('inf')
+            op2 = l2.val if l2 else float('inf')
+            got = min(op1, op2)
+            dump.next = ListNode(got)
+            dump = dump.next
+            if op1 <= op2:
+                l1 = l1.next
+            else:
+                l2 = l2.next
+            
+
+        return head.next
