5a21e4efc805c75e19492277bdf6545cc5b8861a
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-12 15:34:20 +0200

    batch7: update 92-reverse-linked-list-II.py

--- batch7/92-reverse-linked-list-II.py
+++ batch7/92-reverse-linked-list-II.py
@@ -5,5 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-	pass
+        dummy = ListNode()
+        dummy.next = head
+        source = dummy
 
+        # Pass1: Get the source node before the left position.
+        for _ in range(left-1):
+            source = source.next
+
+        # Pass2: Reverse the list starting from left to right inclusive.
+        prev, current = None, source.next
+        for _ in range(right-left+1):
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+    
+        # Pass3: Connect the reversed part with the rest of the list.
+        source.next.next = current
+        source.next = prev
+
+        return dummy.next
