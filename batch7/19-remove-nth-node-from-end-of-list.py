f642f077eb1b9e4c7721a19091eedcff63851555
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-12 20:21:16 +0200

    batch7: update 19-remove-nth-node-from-end-of-list.py

--- batch7/19-remove-nth-node-from-end-of-list.py
+++ batch7/19-remove-nth-node-from-end-of-list.py
@@ -5,5 +5,27 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-	pass
- 
+        # Pass 1: Calculate the length of the linkedlist
+        total = 0
+        current = head
+        while current:
+            total += 1
+            current = current.next
+
+        # Pass 2: Get start index of the node to remove from start position
+        node_to_remove_index = total - n
+        if node_to_remove_index == 0:
+            return head.next
+
+        i = 0
+        prev, current = None, head
+        while current:
+            if i == node_to_remove_index-1:
+                prev = current
+            if i == node_to_remove_index:
+                prev.next = current.next
+                current.next = None
+            current = current.next
+            i += 1
+        
+        return head
