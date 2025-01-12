43fd209f717c78b9373b25f70773a90887d3089b
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-13 00:36:17 +0200

    batch7: update 61-rotate-list.py

--- batch7/61-rotate-list.py
+++ batch7/61-rotate-list.py
@@ -5,4 +5,25 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        pass
+        if not head or not head.next or not k:
+            return head
+
+        # Pass 1: Compute the length of list.
+        length = 1
+        current = head
+        while current.next:
+            current = current.next
+            length += 1
+        
+        # Pass 2: Make the list circular.
+        current.next = head
+
+        # Pass 3: Compute the new tail and new head.
+        k = k % length
+        tail = head
+        for _ in range(length-k-1):
+            tail = tail.next
+        head = tail.next
+        tail.next = None
+
+        return head
