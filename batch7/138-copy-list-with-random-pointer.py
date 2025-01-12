61d45188f62670b1e9fd46f6d74e763997a57168
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-12 15:34:09 +0200

    batch7: update 138-copy-list-with-random-pointer.py

--- batch7/138-copy-list-with-random-pointer.py
+++ batch7/138-copy-list-with-random-pointer.py
@@ -9,5 +9,30 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-	pass
- 
+        if not head:
+            return None
+        
+        # Pass 1: Create twin list alongside the original list.
+        current = head
+        while current:
+            new_node = Node(current.val, current.next, None)
+            current.next = new_node
+            current = new_node.next
+
+        # Pass 2: Assign random pointers to the twin list with the same list state.
+        current = head
+        while current:
+            if current.random:
+                current.next.random = current.random.next
+            current = current.next.next
+    
+        # Pass 3: Seperate the twin list from the original list and vice versa.
+        current, new_head = head, head.next
+        while current:
+            new_node = current.next
+            current.next = new_node.next
+            if new_node.next:
+                new_node.next = new_node.next.next
+            current = current.next
+
+        return new_head
