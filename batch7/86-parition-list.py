5d6a5e24537d1f85c7e9c33416f7c4c363f879e3
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-13 02:15:07 +0200

    batch7: update 86-partition-list.py

--- batch7/86-parition-list.py
+++ batch7/86-parition-list.py
@@ -5,4 +5,18 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        pass
+        less_head, greater_head = ListNode(), ListNode()
+        less, greater = less_head, greater_head
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        greater.next = None
+        less.next = greater_head.next
+
+        return less_head.next
