d3d8794b00380ae2e4f219c09f6c5495f945ecbc
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-15 23:50:20 +0200

    batch8: update 101-symmetric-tree.py

--- batch8/101-symmetric-tree.py
+++ batch8/101-symmetric-tree.py
@@ -6,4 +6,12 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        pass
+        def is_symmetric_sides(left_tree, right_tree):
+            if not left_tree and not right_tree:
+                return True
+            if not left_tree or not right_tree:
+                return False
+            if left_tree.val != right_tree.val:
+                return False
+            return is_symmetric_sides(left_tree.left, right_tree.right) and is_symmetric_sides(left_tree.right, right_tree.left)
+        return is_symmetric_sides(root.left, root.right)
