c76ae0c0f1561985c837d9742ae57aa4a28c329f
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-16 00:01:24 +0200

    batch7: move 100-same-tree.py here

--- batch7/100-same-tree.py
+++ batch7/100-same-tree.py
@@ -6,4 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        pass
+        def sameTree(p1, q1):
+            if not p1 and not q1:
+                return True
+            if not p1 or not q1:
+                return False
+            if p1.val != q1.val:
+                return False
+            return sameTree(p1.left, q1.left) and sameTree(p1.right, q1.right)
+        return sameTree(p, q)
