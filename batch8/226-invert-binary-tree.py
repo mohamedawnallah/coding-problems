e5ccd6b0f586ffc8e1dc42670c062cc6ca4eb922
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-01-25 23:41:31 +0200

    batch8: update 226-invert-binary-tree.py

--- batch8/226-invert-binary-tree.py
+++ batch8/226-invert-binary-tree.py
@@ -6,4 +6,9 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        if not root:
+            return
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
